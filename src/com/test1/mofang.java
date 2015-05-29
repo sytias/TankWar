349993d4b7bdbbcecce533d1b75e31b92022d3a4
sytias <cyc870830@gmail.com>
2015-05-29 00:13:33 -0400

    first

---
+++ src/com/test1/mofang.java
@@ -0,0 +1,18 @@
+package com.test1;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class mofang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
+class Tank2{
+	
+}
